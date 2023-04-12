b470ca34d7d3867d6c81d43c622d3ca6b8e64ba2
Bryan Campos <bryanjose.185637@gmail.com>
2023-04-12 15:46:15 -0600

    merge

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {MyStack} from './src/navigations';
+import {PermissionsProvider} from './src/context/PermissionsContext';
 
 export default function App() {
   return (
-    <NavigationContainer>
-
-    </NavigationContainer>
+    <PermissionsProvider>
+      <NavigationContainer>
+        <MyStack />
+      </NavigationContainer>
+    </PermissionsProvider>
   );
 }
